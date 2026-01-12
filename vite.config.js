9a8176ceea8005f62865076f6ada7001fabca058
anshukr-576 <anshuking576@gmail.com>
2026-01-12 20:36:43 +0530

    fix vite base path for dev and github pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: '/PortFolio/',
+//    resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+export default defineConfig(({ command }) => ({
   plugins: [react(), tailwindcss()],
-   resolve: {
+  base: command === "build" ? "/PortFolio/" : "/",
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
+
